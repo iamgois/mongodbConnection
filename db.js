ea62ae3fe792e28e4a364fc23e02829bba0226cc
Thiago Gois <thiagogoisjunior@gmail.com>
2023-06-02 22:37:03 -0300

    login system

--- db.js
+++ db.js
@@ -1,46 +1,59 @@
-const { MongoClient } = require('mongodb')
-
-let dbConnection
+const mysql = require('mysql2');
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
 module.exports = {
-    connectToDb: (cb) => {
-        //making connection with database
-        MongoClient.connect('mongodb://127.0.0.1:27017')
-            .then((client) => {
-                // connection test
-                dbConnection = client.db()
-                // returning
-                return cb()
-            })
-            .catch(err => {
-                console.log(err)
-                return cb(err)
-            })
-    },
-    getDb: () => dbConnection,
-    findUser: (cb) => {
-        //Making Connection with Database
-        MongoClient.connect('mongodb://127.0.0.1:27017')
-            .then(async (client) => {
-                
-                try {
-                    // Fouding a Database
-                    const database = client.db("users");
-                    // declareted of collection in "users" database
-                    const movies = database.collection("users");
-                    // Query for a user that has the name 'Ervin Howell'
-                    const query = { name: 'Ervin Howell' };
-                    const user = await movies.findOne(query);
-                    // since this method returns the matched document, not a cursor, print it directly
-                    console.log(user.email);
-                  } finally {
-                    // Closing the connection
-                    await client.close();
-                  }
 
-            })
-            .catch(err => {
-                // if have error, push to console
-                console.log(err);
-            })
-    },}
+    connectToDb: () => {
+        var con = mysql.createConnection({
+            host: process.env.host,
+            user: process.env.user,
+            password: process.env.password,
+            database: process.env.database
+          });
+          
+          con.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+          });
+        
+        con.query(
+            "SELECT * FROM usuarios_crassus WHERE name = 'Thiago Gois'",
+            function(err, results, fields){
+                if(err) throw err;
+                console.log(results)
+            }
+        )
+    },
+    loginTest: (cb) => {
+      var con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "T2h60781@",
+        database: 'crassus'
+      });
+
+      con.query('SELECT * FROM usuarios_crassus WHERE email = "' + cb.email + '" AND senha = "' + cb.senha + '"', function (err, result) {
+        if (err) throw err;
+        if(result.length === 0){
+          cb('/login')
+        } else{
+          cb('/home')  
+        }
+      });
+    },
+    createUser: (cb) => {
+      var con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "T2h60781@",
+        database: 'crassus'
+      });
+
+      con.query(`INSERT INTO usuarios_crassus (name, email, senha, facebook_key) VALUES ('`+ cb.name +`', '`+ cb.email +`', '`+ cb.senha +`', '`+ cb.facebook_key +`')`, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result)
+      });
+    }
+  }
--- index.js
+++ index.js
@@ -1,29 +1,115 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { connectToDb, getDb, findUser } = require('./db')
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/";
+const { connectToDb, loginTest, createUser } = require('./db')
+const path = require('path');
+var bodyParser = require('body-parser')
+const mysql = require('mysql2')
 
-// db connection
-let db
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/views')));
 
-connectToDb(async (err) => {
-    if(!err){
-        const users = await findUser();
-        app.listen(3001, () => console.log(users))
+// Adicionando o middleware bodyParser para analisar o corpo da requisição
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/views/index.html'))
+});
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname+'/views/login.html'))
+});
+
+app.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname+'/views/home.html'))
+});
+
+const dbConfig = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+};
+
+// Rota para lidar com o envio do formulário de login
+app.post('/login', (req, res) => {
+  // Obtendo os dados do formulário de login
+  const email = req.body.email;
+  const senha = req.body.senha;
+
+  // Criando uma conexão com o banco de dados
+  const connection = mysql.createConnection(dbConfig);
+
+  // Consulta SQL para verificar o usuário no banco de dados
+  const query = `SELECT * FROM usuarios_crassus WHERE email = '${email}' AND senha = '${senha}'`;
+
+  // Executando a consulta SQL
+  connection.query(query, (err, result) => {
+    if (err) {
+      // Lidar com erros de consulta
+      console.error(err);
+      res.status(500).send('Erro ao realizar a consulta.');
+    } else {
+      if (result.length === 0) {
+        res.redirect('/login');
+      } else {
+        // Usuário encontrado, redirecionar para a página inicial
+        res.redirect('/home');
+      }
     }
-    db = getDb()
-})
 
-app.get('/', (req, res) => {
-    res.send(`<h1>teste</h1>`)
-})
+    // Fechando a conexão com o banco de dados
+    connection.end();
+  });
+});
 
-app.get('/users', (req, res) => {
-    res.json('Rodando!')
-})
+// Rota para lidar com o envio do formulário de login
+app.post('/register', (req, res) => {
+  // Obtendo os dados do formulário de login
+  const name = req.body.name;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const facebook_key = req.body.facebook_key;
 
+  // Criando uma conexão com o banco de dados
+  const connection = mysql.createConnection(dbConfig);
+
+  // Consulta SQL para verificar o usuário no banco de dados
+  const query = `SELECT * FROM usuarios_crassus WHERE email = '${email}'`;
+  const createUserQuery = `INSERT INTO usuarios_crassus (name, email, senha, facebook_key) VALUES ('${name}','${email}','${senha}','${facebook_key}')`
+
+  // Executando a consulta SQL
+  connection.query(query, (err, result) => {
+    if (err) {
+      // Lidar com erros de consulta
+      console.error(err);
+      res.status(500).send('Erro ao realizar a consulta.');
+    } else {
+      if (result.length === 0) {
+        // Usuário não encontrado
+        connection.query(createUserQuery,(err, result) => {
+          if(err) throw err
+          res.redirect('/home')
+        })
+      } else {
+        // Usuário encontrado, redirecionar para a página inicial
+        res.send('Usuário já cadastrado, tente outro!');
+      }
+    }
+
+    // Fechando a conexão com o banco de dados
+    connection.end();
+  });
+});
+
+// Rota para a página inicial após o login
+app.get('/home', (req, res) => {
+  res.send('<h1>Página Inicial</h1>');
+});
+
+// Iniciando o servidor
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Running in port ${port}`);
+});
